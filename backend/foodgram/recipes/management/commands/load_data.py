25340d354dec33b6c38f07884ad03ec67ab45d70
solilov <alexandr.solilov@yandex.ru>
2021-12-16 14:54:51 +0300

    data

--- backend/foodgram/recipes/management/commands/load_data.py
+++ backend/foodgram/recipes/management/commands/load_data.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     help = 'load ingredients to DB'
 
     def handle(self, *args, **options):
-        with open('../../data/ingredients.csv',
+        with open('recipes/data/ingredients.csv',
                   encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
