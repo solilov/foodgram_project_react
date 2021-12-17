54dd9a862cb88b4d6b1282559f66a19241cdeb55
solilov <alexandr.solilov@yandex.ru>
2021-12-17 17:53:41 +0300

    правка фильтр

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,7 +1,14 @@
+from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
+
+from recipes.models import Recipe
+
+User = get_user_model()
 
 
 class FavoritedAndShopping_CartFilter(FilterSet):
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
@@ -15,3 +22,7 @@ class FavoritedAndShopping_CartFilter(FilterSet):
         if value and not self.request.user.is_anonymous:
             return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author')
