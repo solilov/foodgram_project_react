93360bf5e9ba3da1fd0d41593ada75c359c81288
solilov <alexandr.solilov@yandex.ru>
2021-12-18 15:55:12 +0300

    filter ingredient

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
+from rest_framework.filters import SearchFilter
 
 from recipes.models import Recipe
 
@@ -13,3 +14,7 @@ class TagOrAuthorFilter(FilterSet):
     class Meta:
         model = Recipe
         fields = ('tags', 'author')
+
+
+class IngredientFilter(SearchFilter):
+    search_param = 'name'
