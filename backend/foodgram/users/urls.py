3360a10a61ddc422fce7689cc21772d5acf4c529
solilov <alexandr.solilov@yandex.ru>
2021-12-18 21:51:56 +0300

    users.views and users.urls

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from djoser import views
 from rest_framework.routers import DefaultRouter
 
-from users.views import FollowViewSet
+from users.views import FollowViewSet, SubscribeView
 
 router = DefaultRouter()
 
@@ -10,6 +10,10 @@ router.register('users', FollowViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('users/<int:id>/subscribe/',
+         SubscribeView.as_view(),
+         name='create_destroy_subs'
+         ),
     path('auth/token/login/', views.TokenCreateView.as_view(), name='login'),
     path('auth/token/logout/', views.TokenDestroyView.as_view(),
          name='logout'),
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -4,6 +4,7 @@ from rest_framework import response, status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 from api.pagination import CustomPagination
 from api.serializers import FollowSerializer
@@ -25,30 +26,53 @@ class FollowViewSet(views.UserViewSet):
                                       context={'request': request})
         return self.get_paginated_response(serializer.data)
 
-    @action(detail=True, methods=['get', 'delete'],
-            permission_classes=[IsAuthenticated])
-    def subscribe(self, request, id):
+
+class SubscribeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id):
         user = request.user
         following = get_object_or_404(User, id=id)
-        if request.method == 'GET':
-            if user == following:
-                return response.Response('Нельзя подписаться на себя',
-                                         status=status.HTTP_400_BAD_REQUEST)
-            elif Follow.objects.filter(user=user,
-                                       following=following).exists():
-                return response.Response('Вы уже подписаны на этого автора',
-                                         status=status.HTTP_400_BAD_REQUEST)
-            obj = Follow.objects.create(user=user, following=following)
-            # serializer = FollowSerializer(obj, data=request.data)
-            # if serializer.is_valid():
-            #     serializer.save()
-            #     return response.Response(
-            #         serializer.data,
-            #         status=status.HTTP_201_CREATED
-            #     )
-            serializer = FollowSerializer(obj)
-            return response.Response(serializer.data,
-                                     status=status.HTTP_201_CREATED)
-        if request.method == 'DELETE':
-            Follow.objects.filter(user=user, following=following).delete()
-            return response.Response(status=status.HTTP_204_NO_CONTENT)
+        if user == following:
+            return response.Response('Нельзя подписаться на себя',
+                                     status=status.HTTP_400_BAD_REQUEST)
+        elif Follow.objects.filter(user=user,
+                                   following=following).exists():
+            return response.Response('Вы уже подписаны на этого автора',
+                                     status=status.HTTP_400_BAD_REQUEST)
+        obj = Follow.objects.create(user=user, following=following)
+        serializer = FollowSerializer(obj)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
+    def delete(self, request, id):
+        user = request.user
+        following = get_object_or_404(User, id=id)
+        Follow.objects.filter(user=user, following=following).delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    # @action(detail=True, methods=['get', 'delete'],
+    #         permission_classes=[IsAuthenticated])
+    # def subscribe(self, request, id):
+    #     user = request.user
+    #     following = get_object_or_404(User, id=id)
+    #     if request.method == 'GET':
+    #         if user == following:
+    #             return response.Response('Нельзя подписаться на себя',
+    #                                      status=status.HTTP_400_BAD_REQUEST)
+    #         elif Follow.objects.filter(user=user,
+    #                                    following=following).exists():
+    #             return response.Response('Вы уже подписаны на этого автора',
+    #                                      status=status.HTTP_400_BAD_REQUEST)
+    #         obj = Follow.objects.create(user=user, following=following)
+    #         serializer = FollowSerializer(obj)
+    #         return response.Response(serializer.data,
+    #                                  status=status.HTTP_201_CREATED)
+    #     if request.method == 'DELETE':
+    #         Follow.objects.filter(user=user, following=following).delete()
+    #         return response.Response(status=status.HTTP_204_NO_CONTENT)
