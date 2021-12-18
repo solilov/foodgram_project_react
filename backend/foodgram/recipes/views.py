683379189aac5b4280a3aab2a8d590a100e22db5
solilov <alexandr.solilov@yandex.ru>
2021-12-18 14:23:34 +0300

    проверка измененного фильтра и кверисета2

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -40,13 +40,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        queryset = Recipe.objects.all()
+        # queryset = Recipe.objects.all()
         # user = self.request.user
         if self.request.query_params.get('is_favorited'):
-            queryset = queryset.filter(favorites__user=self.request.user)
+            return Recipe.objects.filter(favorites__user=self.request.user)
         elif self.request.query_params.get('is_in_shopping_cart'):
-            queryset = queryset.filter(shopping_cart__user=self.request.user)
-        return queryset
+            return Recipe.objects.filter(shopping_cart__user=self.request.user)
+        return Recipe.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
