a7caa05e47d2f4e09857fe2ff153bf25ff0d68f6
solilov <alexandr.solilov@yandex.ru>
2021-12-19 23:30:55 +0300

    правки4

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from users.models import Follow
 
 User = get_user_model()
 
 
-class UserAdmin(admin.ModelAdmin):
+class New_UserAdmin(UserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name']
     search_fields = ['username', 'email']
     list_filter = ['email', 'username']
 
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, New_UserAdmin)
 admin.site.register(Follow)
